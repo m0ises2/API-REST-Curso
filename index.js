68e12c3fb00e30d95785e2e0bec310e7fc2b2f16
m0ises2 <moisesalvarado84@gmail.com>
2017-03-05 11:55:54 -0400

    Node.js API REST Tutorial #03 - Instalando body-parser y nodemon

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 'use strict'
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+const port = process.env.PORT || 3001;
+
+//Middlewares
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //Ecmascript 6 incorpora la funcionalidad de las arrow functions:
-app.listen(3000, () => {
-  console.log("API REST corriendo en http://localhost:3000");
+app.listen(port, () => {
+  console.log(`API REST corriendo en http://localhost:${port}`);
 })
