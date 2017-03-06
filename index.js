0ba8c35340b711d6e2405801ef039c5ac7ce151e
m0ises2 <moisesalvarado84@gmail.com>
2017-03-06 19:26:05 -0400

    Node.js API REST Tutorial #06 - Usando POSTMAN e implementación de endpoints POST y GET

--- index.js
+++ index.js
@@ -9,9 +9,31 @@ const port = process.env.PORT || 3001;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/hola/:name/:lastname', (req, res) => {
-  res.send({message: `Hola. Tu nombre es ${req.params.name} y tu apellido ${req.params.lastname}`
-  });
+/*
+  Endpoints:
+*/
+//GET:
+app.get('/api/product', (req, res) => {
+  res.status(200).send( { message: 'Todo O.K' } );
+});
+
+app.get('/api/product/:productId', (req, res) => {
+
+});
+
+//POST:
+app.post('/api/product', (req, res) => {
+    console.log(req.body);
+    res.status(200).send( {message: "Producto recibido"} );
+});
+
+//PUT:
+app.put('/api/product/:productId', (req, res) => {
+
+});
+//DELETE:
+app.delete('/api/product/:productId', (req, res) => {
+
 });
 
 //Ecmascript 6 incorpora la funcionalidad de las arrow functions:
